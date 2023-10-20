9dbd8c21ea87df04219ac837bce9a12b4cd76b58
Yaroslav131 <berserk12186@gmail.com>
2023-10-20 21:45:55 +0300

    set env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,43 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'prettier',
+    'airbnb',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'import'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'no-unused-vars': 'off',
+    camelcase: 'off',
+    'import/extensions': 'off', 
+    'import/no-unresolved': 'off', 
+    'react/require-default-props': 'off', 
+    'import/no-extraneous-dependencies': 'off'
+  },
 };
